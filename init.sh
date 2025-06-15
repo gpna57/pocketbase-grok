d39fa623e87adf55ff136b6c3534a6a34d4eee00
gpna57 <31196847+gpna57@users.noreply.github.com>
2025-06-14 22:57:44 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 /pb/pocketbase superuser upsert gabrielpablonew@gmail.com gabriel2002
-exec /pb/pocketbase serve --http=0.0.0.0:8080
+exec /pb/pocketbase serve --http=0.0.0.0:8080 --httpDataDir=/pb/pb_data
